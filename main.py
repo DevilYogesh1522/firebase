e5ab8e6cbbb37cabf9077c855296b7d9356f430f
DevilYogesh1522 <133919650+DevilYogesh1522@users.noreply.github.com>
2024-05-08 00:11:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def root():
     return render_template("index.html", times=dummy_times)
 
 
-if __name__ == "__main__":
+
     # This is used when running locally only. When deploying to Google App
     # Engine, a webserver process such as Gunicorn will serve the app. This
     # can be configured by adding an `entrypoint` to app.yaml.
